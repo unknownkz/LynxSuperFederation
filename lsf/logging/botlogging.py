a7a22babc52326c005b388be4d5cf371a6f38852
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-02 11:13:37 +0700

    Delete botlogging.py

--- lsf/logging/botlogging.py
+++
@@ -1,8 +0,0 @@
-import logging as lsflog
-
-lsflog.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[lsflog.FileHandler("log.txt"), lsflog.StreamHandler()],
-    level=lsflog.INFO,
-)
-LOGGER = lsflog.getLogger(__name__)
