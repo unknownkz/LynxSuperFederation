bc31b2efbe4b1247c3efa14d647d578d39e02131
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-05 02:15:02 +0700

    Update arq.py

--- lsf/utils/arq.py
+++ lsf/utils/arq.py
@@ -1,18 +1,13 @@
 import json
 import sys
+import aiohttp
 from random import randint
 from time import time
-
-import aiohttp
-from lsf import aiohttpsession
 from aiohttp import ClientSession
-
 from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
-
-from lsf import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
-from lsf import pbot
+from lsf import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY, aiohttpsession, fed_lynxbot
 
 # Aiohttp Client
 print("[INFO]: INITIALZING AIOHTTP SESSION")
