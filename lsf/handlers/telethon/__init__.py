82ff7e24ec849b55c306ed64b7091380192b72ea
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-06 18:19:21 +0700

    Create __init__.py

---
+++ lsf/handlers/telethon/__init__.py
@@ -0,0 +1,8 @@
+from ... import DEV_ID, SD_ID, SUPPORT_ID, WHITELIST_ID, TIGERS_ID, lynx_client
+
+
+U_ABSURD = DEV_ID + SD_ID + SUPPORT_ID + WHITELIST_ID + TIGERS_ID
+
+U_ABSURD = (
+    list(DEV_ID) + list(SD_ID) + list(SUPPORT_ID) + list(WHITELIST_ID) + list(TIGERS_ID)
+)
