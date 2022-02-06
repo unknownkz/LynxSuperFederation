cff5d5ec444ab385998e5c24efcf451d3d2eaa8c
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-06 08:30:51 +0700

    Update mongodb.py

--- lsf/mongodb.py
+++ lsf/mongodb.py
@@ -5,19 +5,20 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from lsf import MONGO_DB_URI
 from lsf.configuration import get_int_key, get_str_key
+from lsf import LOGGER as LOGS_LSF
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "LSF"
 
-
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["LSF"]
+
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(LOGS_LSF.critical("Can't connect to mongodb! Exiting..."))
