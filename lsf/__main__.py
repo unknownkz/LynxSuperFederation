f9796dad871d63314ca8fab9a2215d33bfd06135
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-10 19:03:43 +0700

    Update __main__.py

--- lsf/__main__.py
+++ lsf/__main__.py
@@ -273,19 +273,19 @@ def lynx_about_callback(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
     if query.data == "lynx_":
         query.message.edit_text(
-            text="""Hello *{}*, My name is *{}*.
-            A Powerful Telegram Group Management Bot built to help you manage Group easily.
-            \n ‣ I can Restrict Users.
-            \n ‣ I can Greet Users with customizable welcome message and even set a group rules
-            \n ‣ I have an advanced Anti-Flood System which will help you to safe group from Spammmer.
-            \n ‣ I can Warn Users until they reach max Warns, with each predefined actions such as Ban, Mute, Kick, Fban etc.
-            \n ‣ I have Note Keeping System, Blacklists, And even Predetermined replies on certain keywords.
-            \n ‣ I check Admins Permissions before perform any Command and more Stuffs.
-            \n ‣ I have an advanced Artificial Chatbot System, so can talk with users like humans.
-            \n*If you have any Question, You can join Support Chat. My Developer Team will Answer. Check Link Below*
-            \n*Info Server*
-            `Starting from: {}`
-            `and {} users in {} chats.`""".format(
+            text="""Hello *{}*, My name is *{}**
+A Powerful Telegram Group Management Bot built to help you manage Group easily.
+\n ‣ I can Restrict Users.
+\n ‣ I can Greet Users with customizable welcome message and even set a group rules
+\n ‣ I have an advanced Anti-Flood System which will help you to safe group from Spammmer.
+\n ‣ I can Warn Users until they reach max Warns, with each predefined actions such as Ban, Mute, Kick, Fban etc.
+\n ‣ I have Note Keeping System, Blacklists, And even Predetermined replies on certain keywords.
+\n ‣ I check Admins Permissions before perform any Command and more Stuffs.
+\n ‣ I have an advanced Artificial Chatbot System, so can talk with users like humans.
+\n*If you have any Question, You can join Support Chat. My Developer Team will Answer. Check Link Below*
+\n*Info Server*
+`Starting from: {}
+and {} users in {} chats.`""".format(
                 escape_markdown(first_name),
                 escape_markdown(context.bot.first_name),
                 escape_markdown(uptime),
