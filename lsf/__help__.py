896b81ff2ae22ab34207bd9aba27108b562c1352
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-11 17:10:22 +0700

    Update __help__.py

--- lsf/__help__.py
+++ lsf/__help__.py
@@ -10,8 +10,8 @@ from telegram.ext.dispatcher import DispatcherHandlerStop
 from os.path import isfile
 
 
-HELP_STRINGS = """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with. """.format(
+HELP_STRINGS = """Hey there! I'm *{}* 😼.
+I'm a management group focused on the Lynx Federation! Have a look at the following for an idea of some of the things I can help you with. """.format(
     dispatcher.bot.first_name,
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
 )
