030843bd9cc6840f1b9237735922b5da761334fa
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-12 01:30:03 +0700

    Update connection.py

--- lsf/plugins/admins/connection.py
+++ lsf/plugins/admins/connection.py
@@ -304,15 +304,17 @@ def connected(bot: Bot, update: Update, chat, user_id, need_admin=True):
 
 CONN_HELP = """
  Actions are available with connected groups:
- ‣ View and edit Notes.
- ‣ View and edit Filters.
- ‣ Get invite link of chat.
- ‣ Set and control AntiFlood settings.
- ‣ Set and control Blacklist settings.
- ‣ Set Locks and Unlocks in chat.
- ‣ Enable and Disable commands in chat.
- ‣ Export and Imports of chat backup.
- ‣ More in future!"""
+
+1.) View and edit Notes.
+2.) View and edit Filters.
+3.) Get invite link of chat.
+4.) Set and control AntiFlood settings.
+5.) Set and control Blacklist settings.
+6.) Set Locks and Unlocks in chat.
+7.) Enable and Disable commands in chat.
+8.) Export and Imports of chat backup.
+9.) More in future!
+"""
 
 
 def help_connect_chat(update, context):
@@ -381,21 +383,6 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Connect"
-
-__help__ = """
-Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
-This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
-
- ‣ `/connect`: Connects to chat (Can be done in a group by `/connect` or `/connect` <chat id> in PM)
- ‣ `/connection`: List connected chats
- ‣ `/disconnect`: Disconnect from a chat
- ‣ `/helpconnect`: List available commands that can be used remotely
-
-*Admins only:*
- ‣ `/allowconnect <yes/no>`: allow a user to connect to a chat
-"""
-
 CONNECT_CHAT_HANDLER = CommandHandler(
     "connect", connect_chat, pass_args=True, run_async=True
 )
@@ -417,3 +404,18 @@ dispatcher.add_handler(DISCONNECT_CHAT_HANDLER)
 dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
 dispatcher.add_handler(HELP_CONNECT_CHAT_HANDLER)
 dispatcher.add_handler(CONNECT_BTN_HANDLER)
+
+__help__ = """
+Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
+This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
+
+ • /connect : Connects to chat (Can be done in a group by `/connect` or `/connect` <chat id> in PM)
+ • /connection : List connected chats
+ • /disconnect : Disconnect from a chat
+ • /helpconnect : List available commands that can be used remotely
+
+*Admins only:*
+ • /allowconnect <yes/no> : allow a user to connect to a chat
+"""
+
+__mod_name__ = "Connect"
