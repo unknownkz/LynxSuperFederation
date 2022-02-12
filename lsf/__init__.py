ec4dfea529fb0753d364022be1e2c9ea68cd1d96
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-13 00:21:01 +0700

    Update __init__.py

--- lsf/__init__.py
+++ lsf/__init__.py
@@ -44,9 +44,9 @@ if not os.sys.platform.startswith("linux") and not os.sys.build_platform.startsw
 if os.sys.version_info.major[0] < 3 or os.sys.version_info.minor[1] < 10 or os.sys.version_info.micro[2] < 2:
     LOGGER.error(
         "You have to use python version of at least {major}.{minor}.{micro} ! quitting.."
-    ).format(sys.version_info[0],
-             sys.version_info[1],
-             sys.version_info[2])
+    ).format(sys.version_info.major,
+             sys.version_info.minor,
+             sys.version_info.micro)
     os.system('clear') and os.sys.exit(1)
 
 ENV = bool(os.environ.get("ENV", False))
