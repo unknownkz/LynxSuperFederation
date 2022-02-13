8cce8cc4797ce0271c766d6fe54307365ca0ea92
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-13 20:39:02 +0700

    ...

--- lsf/plugins/controllers.py
+++ lsf/plugins/controllers.py
@@ -1,6 +1,3 @@
-import os
-import subprocess
-import sys
 import lsf
 
 from contextlib import suppress
@@ -18,7 +15,7 @@ def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
         state = "Lockdown is " + "on" + "yes" if not lsf.ALLOW_CHATS else "off" + "no"
-        update.effective_message.reply_text("Current state: {}").format(state())
+        update.effective_message.reply_text("Current state: {}".format(state())
         return
     if args[0].lower() in ["off", "no"]:
         lsf.ALLOW_CHATS = True
@@ -49,46 +46,11 @@ def leave(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Send a valid chat ID")
 
 
-@dev_plus
-def gitpull(update: Update, context: CallbackContext):
-    lynx_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart.",
-    )
-    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
-
-    lynx_msg_text = lynx_msg.text + "\n\nPulling... "
-
-    for i in reversed(range(5)):
-        lynx_msg.edit_text(lynx_msg_text + str(i + 1))
-        sleep(1)
-
-    lynx_msg.edit_text("Restarted.")
-
-    os.system("lynx_restart.bat")
-    os.execv("lynx_start.bat", sys.argv)
-
-
-@dev_plus
-def rebooting(update: Update, context: CallbackContext):
-    lynx_msg = update.effective_message.reply_text(
-        "Restarting... please wait!",
-    )
-    os.system("lynx_restart.bat")
-    os.execv("lynx_start.bat", sys.argv)
-    lynx_msg.edit_text("Done.. I've come back to life")
-
-
-
-
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
-GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
-REBOOTING_HANDLER = CommandHandler("rebooting", rebooting, run_async=True)
 ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups, run_async=True)
 
 dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 dispatcher.add_handler(LEAVE_HANDLER)
-dispatcher.add_handler(GITPULL_HANDLER)
-dispatcher.add_handler(REBOOTING_HANDLER)
 
 __mod_name__ = "Controllers"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, REBOOTING_HANDLER, ALLOWGROUPS_HANDLER]
