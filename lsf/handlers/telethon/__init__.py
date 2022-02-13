d3e3a8917bcc03674a223187391a19c8aaec6816
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-14 04:55:14 +0700

    Ohh iya bang gini aja

--- lsf/handlers/telethon/__init__.py
+++ lsf/handlers/telethon/__init__.py
@@ -1,7 +1,3 @@
 from ... import DEV_ID, SD_ID, SUPPORT_ID, TIGERS_ID, WHITELIST_ID, lynx_client
 
-xxx = {"DEV_ID", "SD_ID", "SUPPORT_ID", "WHITELIST_ID", "TIGERS_ID"}
-
-zzz = list(set(DEV_ID) | set(SD_ID) | set(SUPPORT_ID) | set(WHITELIST_ID) | set(TIGERS_ID))
-
-U_ABSURD = xxx.union(zzz)
+U_ABSURD = list(set().union(DEV_ID, SD_ID, SUPPORT_ID, WHITELIST_ID, TIGERS_ID))
