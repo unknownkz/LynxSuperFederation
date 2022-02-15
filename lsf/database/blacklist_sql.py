54205b32a65c621c236cf8920461509fc5c75c8b
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-15 07:10:56 +0700

    ...

--- lsf/database/blacklist_sql.py
+++ lsf/database/blacklist_sql.py
@@ -334,7 +334,7 @@ def __load_chat_blacklists():
         for xx in all_filters:
             CHAT_BLACKLISTS[xx.chat_id] += [xx.trigger]
 
-        CHAT_BLACKLISTS = {xx: add(p) for xx, p in CHAT_BLACKLISTS.items()}
+        CHAT_BLACKLISTS = {xx: add(p) for xx, p in CHAT_BLACKLISTS.update()}
 
     finally:
         SESSION.close()
