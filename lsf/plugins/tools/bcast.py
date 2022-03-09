003657a7f43ca2bbbcbb5ad00cd4c558af742dc1
unknownkz <iamkenzo404@gmail.com>
2022-03-09 15:04:53 +0700

    ...
    ...

--- lsf/plugins/tools/bcast.py
+++ lsf/plugins/tools/bcast.py
@@ -2,14 +2,12 @@
 from random import randrange
 from time import sleep
 
-from telegram import Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import TelegramError
-from telegram.ext import CallbackContext, CallbackQueryHandler
-from telegram.ext.dispatcher import run_async
+from telegram.ext import CallbackContext
 
-from ... import LOGGER, dispatcher
+from ... import LOGGER
 from ...database import users_sql as sql
-from ...handlers.misc import EqInlineKeyboardButton
 from ...handlers.valid import is_user_admin
 from ...handlers.valid import bot_admin as absolute
 from ..commander import Lynxcmd
@@ -17,130 +15,35 @@ from ..commander import Lynxcmd
 CHAT_GROUP = 30
 
 
-keyboard1 = [
-    [
-        InlineKeyboardButton(text="Verify Now", callback_data="verify_admin"),
-        InlineKeyboardButton(text="Cancel", callback_data="verify_cancel"),
-    ]
-]
-
-"""def key_build(*args, **kwargs, chat=None) -> List:
-    if not chat:
-         buttons = (
-            [
-                EqInlineKeyboardButton(
-                    bcast,
-                    callback_data="verify_ads",
-                )
-            ]
-        )
-    else:
-        buttons = (
-            [
-                EqInlineKeyboardButton(
-                    bcast
-                    callback_data="verify_ads",
-                )
-            ]
-        )
-"""
-
-VERIFY_TEXT = "Click the button below if you want to use the broadcast feature."
-
-
-def verify_ads(chat_id, text, keyboard=None):
-    if not keyboard:
-        keyboard = InlineKeyboardMarkup(keyboard1)
-    dispatcher.bot.send_message(
-        chat_id=chat_id,
-        text=text,
-        parse_mode=ParseMode.MARKDOWN,
-        disable_web_page_preview=False,
-        reply_markup=keyboard,
-    )
-
-
-def verify_admins_call(update: Update, context: CallbackContext):
-    user = update.effective_user
-    chat = update.effective_chat
-    query = update.callback_query
-    if query.data == "verify_admin":
-        xx = is_user_admin(chat, user.id)
-        if not xx:
-            update.effective_message.reply_text(
-                text="Sorry, u're not admin.",
-                reply_markup=ReplyKeyboardRemove(),
-                parse_mode=ParseMode.MARKDOWN,
-                timeout=30,
-            )
-        else:
-            update.effective_message.reply_text(
-                text="✅Succesfully\nNow send message to broadcast.\n/bcadmin <text>",
-                reply_markup=ReplyKeyboardRemove(),
-                parse_mode=ParseMode.MARKDOWN,
-                timeout=30,
-            )
-
-    elif query.data == "verify_cancel":
-        hxz = update.effective_message
-        msg = hxz.reply_text(
-            text="Broadcast cancelled.",
-            reply_markup=ReplyKeyboardRemove(),
-        )
-        sleep(4)
-        msg.delete()
-
-    args = context.args
-    if len(args) >= 2:
-        to_group = False
-    if args[0].lower() == "bcadmin":
-        to_group = True
-    else:
-        to_group = True
-        return
-
-    sending = msg.text.split(None, 1)
-    if len(sending) >= 2:
-        chats = sql.get_all_chats() or []
-        succ = failed = 0
-
-    if to_group:
-        for xz in chats:
-            try:
-                context.bot.sendMessage(
-                    int(xz.chat_id),
-                    sending[1],
-                )
-                sleep(randrange(2, 4))
-                succ += 1
-            except TelegramError:
-                failed += 1
-                LOGGER.warning(
-                    "Couldn't send broadcast to %s, group name %s",
-                    str(xz.chat_id),
-                    str(xz.chat_name),
-                )
-
-        msg.reply_photo(
-            photo="https://ibb.co/vjtp4tW",
-            quote=True or False,
-            caption="Broadcast complete.\n❎ Failed: {} groups.\n✅ Success: {} groups.".format(failed, succ),
-            parse_mode=ParseMode.HTML,
-        )
-        sleep(4)
-        msg.delete()
-
-
-@Lynxcmd("bcast")
+@Lynxcmd("bcast", group=CHAT_GROUP)
 @absolute
 def broadcasts(update: Update, context: CallbackContext):
     wx = update.effective_message
     user = update.effective_user
     chat = update.effective_chat
-    verify_ads(update.effective_chat.id, VERIFY_TEXT)
+    xx = is_user_admin(chat, user.id)
+    if not xx:
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Help ❔", urrl="t.me/lynxsfrobot?start=help"),
+                ],
+                [
+                    InlineKeyboardButton(text="Lynx News", url="https://t.me/LynxUpdates"),
+                ],
+            ]
+        )
+        wx.reply_photo(
+            photo="https://ibb.co/9V51wSC",
+            quote=True or False,
+            caption="For more information, please click the button below.",
+            reply_markup=keyboard,
+            parse_mode=ParseMode.MARKDOWN,
+        )
+        sleep(10)
+        wx.delete()
 
-    """
-    sending = wx.text.split(None, 1)
+    sending = wx.text.split(None, 1) | wx.photo.split(None, 1) | wx.sticker.split(None, 1) | wx.audio.split(None, 1)
     if len(sending) >= 2:
         chats = sql.get_all_chats() or []
         succ = failed = 0
@@ -160,21 +63,24 @@ def broadcasts(update: Update, context: CallbackContext):
                     str(xz.chat_name),
                 )
 
-        message = update.effective_message
-        ujang = message.reply_photo(
+        mesg = update.effective_message
+        keyboard1 = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Lynx News", url="https://t.me/LynxUpdates"),
+                ],
+            ]
+        )
+        mesg.reply_photo(
             photo="https://ibb.co/vjtp4tW",
             quote=True or False,
             caption="Broadcast complete.\n❎ Failed: {} groups.\n✅ Success: {} groups.".format(failed, succ),
-            parse_mode=ParseMode.HTML,
+            reply_markup=keyboard1,
+            parse_mode=ParseMode.MARKDOWN,
         )
-        sleep(4)
-        ujang.delete()
-    """
+        sleep(10)
+        mesg.delete()
 
-
-bcast_callback_handler = CallbackQueryHandler(verify_admins_call, pattern=r"bcadmin", run_async=True)
-
-dispatcher.add_handler(bcast_callback_handler)
 
 __mod_name__ = "Broadcast"
 
